e043a4474d336d7b6177ab14e493202c1d143340
Armel FORTUN <armel.fortuna@tchack.com>
2026-03-17 12:07:15 +0100

    Force Yes to xcopy

--- copy.bat
+++ copy.bat
@@ -1,5 +1,5 @@
 cd /D %USERPROFILE%\AppData\Roaming\krita\
-xcopy /S %~dp0\exportLayerAnim "pykrita\exportLayerAnim"
-xcopy %~dp0\exportLayerAnim.desktop "pykrita\exportLayerAnim.desktop"
+xcopy /S %~dp0\exportLayerAnim "pykrita\exportLayerAnim" /Y
+xcopy %~dp0\exportLayerAnim.desktop "pykrita\exportLayerAnim.desktop" /Y
 mkdir "actions"
-xcopy %~dp0\exportLayerAnim.action "actions\exportLayerAnim.action"
+xcopy %~dp0\exportLayerAnim.action "actions\exportLayerAnim.action" /Y
